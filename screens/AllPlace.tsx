badc46a405934f7fff8cb6829b96ba3e015d4cdc
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-05 21:48:14 +0300

    feat: add screens

---
+++ screens/AllPlace.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AllPlace() {
+  return (
+    <View>
+      <Text>AllPlace</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
