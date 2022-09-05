9d2ffc8c432f582df4e23398581114daa71649b7
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-05 23:48:05 +0300

    feat: add Global Color and styling for navigator

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import AllPlaces from './screens/AllPlaces';
 import AddPlace from './screens/AddPlace';
 import IconButton from './components/UI/IconButton';
+import { Colors } from './constants/colors';
 
 export type RootStackParamList = {
   AllPlaces: undefined;
@@ -20,11 +21,16 @@ export default function App() {
     <>
       <StatusBar style="auto" />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: Colors.primary500 },
+          headerTintColor: Colors.gray700,
+          contentStyle: { backgroundColor: Colors.gray700 },
+        }}>
           <Stack.Screen
             name="AllPlaces"
             component={AllPlaces}
             options={({ navigation }) => ({
+              title: 'Your Favorite Places',
               headerRight: ({ tintColor }) => (
                 <IconButton
                   icon={'add'}
@@ -34,7 +40,9 @@ export default function App() {
                 />
               )
             })} />
-          <Stack.Screen name="AddPlace" component={AddPlace} />
+          <Stack.Screen name="AddPlace" component={AddPlace} options={{
+            title: 'Add a new Place'
+          }} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
