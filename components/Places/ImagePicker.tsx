b433f3951abaebee407fb33676c2240436492855
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-06 20:47:56 +0300

    feat: add and configure the camera package

---
+++ components/Places/ImagePicker.tsx
@@ -0,0 +1,51 @@
+import { Alert, Button, StyleSheet, View } from 'react-native';
+import React from 'react';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+
+export default function ImagePicker() {
+  const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
+
+  async function verifyPermissions() {
+
+    if (cameraPermissionInformation!.status === PermissionStatus.UNDETERMINED) {
+      const permissionResponse = await requestPermission();
+
+      return permissionResponse.granted
+    }
+
+    if (cameraPermissionInformation!.status === PermissionStatus.DENIED) {
+      Alert.alert(
+        'Insufficient Permissions!',
+        'You need to grant camera permissions to use this app'
+      );
+      return false;
+    }
+
+    return true;
+  }
+
+  async function TakeImageHandler() {
+    const hasPermission = await verifyPermissions();
+
+    if (!hasPermission) {
+      return;
+    }
+
+    const image = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.5
+    });
+    console.log(image);
+  }
+
+  return (
+    <View>
+      <View>
+      </View>
+      <Button title='Take Image' onPress={TakeImageHandler} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
