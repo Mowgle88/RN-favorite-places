eb4f856b05a8298f6679579bf9d653b888a6040d
Mowgle <84435130+Mowgle88@users.noreply.github.com>
2022-09-09 23:05:37 +0300

    feat: add styles to place items

--- components/Places/PlaceItem.tsx
+++ components/Places/PlaceItem.tsx
@@ -1,6 +1,7 @@
 import { Pressable, StyleSheet, Text, View, Image } from 'react-native';
 import React from 'react';
 import { Place } from '../../models/place';
+import { Colors } from '../../constants/colors';
 
 interface PlaceItemProps {
   place: Place,
@@ -9,14 +10,49 @@ interface PlaceItemProps {
 
 export default function PlaceItem({ place, onSelect }: PlaceItemProps) {
   return (
-    <Pressable onPress={onSelect}>
-      <Image source={{ uri: place.imageUri }} />
-      <View>
-        <Text>{place.title}</Text>
-        <Text>{place.address}</Text>
+    <Pressable style={({ pressed }) => [styles.item, pressed && styles.pressed]} onPress={onSelect}>
+      <Image style={styles.image} source={{ uri: place.imageUri }} />
+      <View style={styles.info}>
+        <Text style={styles.title}>{place.title}</Text>
+        <Text style={styles.adress}>{place.address}</Text>
       </View>
     </Pressable>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  item: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    borderRadius: 6,
+    marginVertical: 12,
+    backgroundColor: Colors.primary500,
+    elevation: 2,
+    shadowColor: 'black',
+    shadowOpacity: 0.15,
+    shadowOffset: { width: 1, height: 1 },
+    shadowRadius: 2,
+  },
+  pressed: {
+    opacity: 0.7,
+  },
+  image: {
+    flex: 1,
+    borderBottomLeftRadius: 4,
+    borderTopLeftRadius: 4,
+    height: 100,
+  },
+  info: {
+    flex: 2,
+    padding: 12
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: Colors.gray700
+  },
+  adress: {
+    fontSize: 12,
+    color: Colors.gray700
+  }
+})
